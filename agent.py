24e02296a2fb2e5994807e1ee955efb17cb0d490
Hennzau <dev@enzo-le-van.fr>
2023-10-19 15:50:57 +0200

    Agent and Graph

--- agent.py
+++ agent.py
@@ -1,3 +1,22 @@
-"""
+# Carla IMPORTS
 
-"""
+from autoagents.autonomous_agent import AutonomousAgent
+
+
+class Agent(AutonomousAgent):
+
+    def setup(self, destination, path_to_config_file):
+        print("Setup")
+
+    def sensors(self):
+        return []
+
+    def run_step(self, input_data, timestamp):
+
+
+        return VehicleControl(
+            steer=float(0),
+            throttle=float(0),
+            brake=float(0),
+            hand_brake=False,
+        )
